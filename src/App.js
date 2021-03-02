02978ab8ea259653aa156cef99e563f4d7c916d6
Андрей Мельничук <mvdmvn@MacBook-Pro-Andrej.local>
2021-03-02 07:53:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import TodoList from "./components/TodoList";
+import TodoItems from "./components/TodoItems";
+
+class App extends Component {
+  inputElement = React.createRef();
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        { text: "Learn JS", key: 1, done: "not-done" },
+        { text: "Learn React", key: 2, done: "not-done" },
+        { text: "Learn Redux", key: 3, done: "not-done" },
+        { text: "Do Something Interesting", key: 4, done: "not-done" },
+      ],
+      currentItem: {
+        text: "",
+        key: 1,
+        done: "not-done",
+      },
+    };
+  }
+
+  deleteItem = (key) => {
+    const filteredItems = this.state.items.filter((item) => {
+      return item.key !== key;
+    });
+    this.setState({
+      items: filteredItems,
+    });
+  };
+
+  setIsTaskCompleted = (id) => {
+    const taskId = this.state.items.findIndex((arg) => arg.key === id);
+    const currentItem = {
+      text: this.state.items[taskId].text,
+      key: this.state.items[taskId].key,
+      done: "done",
+    };
+    console.log(currentItem);
+    const newTasksList = [
+      ...this.state.items.slice(0, taskId),
+      currentItem,
+      ...this.state.items.slice(taskId + 1),
+    ];
+    this.setState({
+      items: newTasksList,
+    });
+  };
+
+  handleInput = (e) => {
+    const itemText = e.target.value;
+    const currentItem = {
+      text: itemText,
+      key: this.state.items.length + 1,
+      done: "not-done",
+    };
+    this.setState({
+      currentItem,
+    });
+  };
+  addItem = (e) => {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== "") {
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: { text: "", key: "", done: "not-done" },
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem}
+        />
+        <TodoItems
+          entries={this.state.items}
+          setIsTaskCompleted={this.setIsTaskCompleted}
+          deleteItem={this.deleteItem}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class TodoItems extends Component {
+  createTasks = (item) => {
+    let isItemDone = item.done;
+    return (
+      <li className={isItemDone} key={item.text}>
+        {item.text}
+        <div>
+          <button onClick={() => this.props.setIsTaskCompleted(item.key)}>
+            Done
+          </button>
+          <button onClick={() => this.props.deleteItem(item.key)}>X</button>
+        </div>
+      </li>
+    );
+  };
+  render() {
+    const todoEntries = this.props.entries;
+    const listItems = todoEntries.map(this.createTasks);
+
+    return <ul className="theList">{listItems}</ul>;
+  }
+}
+
+export default TodoItems;
